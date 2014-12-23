f53fd9de0cb41ecc11c4f8805817f68990605d7e
Adrian La Duca <aladuca@summatus.net>
2014-12-23 11:31:47 -0500

    Fix slashes for URL (used for report filename path)

--- RptPutty/App_Start/RouteConfig.cs
+++ RptPutty/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace RptPutty
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}/{*id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
