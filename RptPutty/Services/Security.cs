c86d0588319a2a2d5c9b88fd76c9de63fb19379d
Adrian La Duca <aladuca@summatus.net>
2015-05-15 14:08:41 -0400

    Changed report generation status to only show reports that the user has generated (admin's see all generated reports)

---
+++ RptPutty/Services/Security.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RptPutty.Services
+{
+    public class Security
+    {
+        public bool getAdmin(String userName)
+        {
+            bool? isAdmin = false;
+            if (string.IsNullOrEmpty(userName)) { return (bool) isAdmin; }
+            using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["RptPutty"].ConnectionString))
+            {
+                SqlCommand sqlcmd = new SqlCommand("SELECT [ADMIN] FROM [USERS] WHERE USERNAME = @USER", sqlcon);
+                sqlcmd.Parameters.Add(new SqlParameter("@USER", userName));
+                
+                try
+                {
+                    sqlcon.Open();
+                    isAdmin = (bool)sqlcmd.ExecuteScalar();
+                }
+                catch { }
+            }
+            if (!isAdmin.HasValue) { isAdmin = false; }
+            return (bool) isAdmin;
+        }
+    }
+}
