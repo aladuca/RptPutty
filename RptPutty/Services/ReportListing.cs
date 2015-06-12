03f9b13e5bd3c2d7a2b67e5c1102fcce787df6bc
Adrian La Duca <aladuca@summatus.net>
2015-06-12 10:49:10 -0400

    Modify external report listing query to be an application setting.

--- RptPutty/Services/ReportListing.cs
+++ RptPutty/Services/ReportListing.cs
@@ -16,13 +16,7 @@ namespace RptPutty.Services
             List<Listing> list = new List<Listing>();
             using (OdbcConnection oconn = new OdbcConnection(ConfigurationManager.ConnectionStrings["UserAccess"].ConnectionString))
             {
-                OdbcCommand ocomm = new OdbcCommand("SELECT DISTINCT X_FILE_NAME, CLARITY_RPT.REPORT_NAME " +
-                    "FROM X_CLARITY_SUBSCRIB "+
-                    "INNER JOIN CLARITY_EMP ON X_CLARITY_SUBSCRIB.USER_NUMBER_ID=CLARITY_EMP.USER_ID "+
-                    "LEFT JOIN REPORT_INFO ON X_CLARITY_SUBSCRIB.SUBSCRIBED_REPT_ID = REPORT_INFO.REPORT_INFO_ID " +
-                    "LEFT JOIN TEMPLATE_INFO ON REPORT_INFO.REPORT_ID  = TEMPLATE_INFO.REPORT_ID " +
-                    "LEFT JOIN CLARITY_RPT ON REPORT_INFO.REPORT_ID = CLARITY_RPT.ASSOC_REPORT_ID " +
-                    "WHERE CLARITY_EMP.SYSTEM_LOGIN=? AND X_FILE_NAME IS NOT NULL AND CLARITY_RPT.REPORT_NAME IS NOT NULL AND TEMPLATE_INFO.STATUS_C = 0");
+                OdbcCommand ocomm = new OdbcCommand(ConfigurationManager.AppSettings["extReportListing"]);
                 ocomm.Parameters.Add(new OdbcParameter("username", username));
                 ocomm.Connection = oconn;
 
@@ -45,6 +39,29 @@ namespace RptPutty.Services
 
             }
             return list;
+        }
+        public Boolean getReportAccess(string username, string fname)
+        {
+            bool reportCount = false;
+            using (OdbcConnection oconn = new OdbcConnection(ConfigurationManager.ConnectionStrings["UserAccess"].ConnectionString))
+            {
+                OdbcCommand ocomm = new OdbcCommand("SELECT DISTINCT X_FILE_NAME, CLARITY_RPT.REPORT_NAME " +
+                    "FROM X_CLARITY_SUBSCRIB " +
+                    "INNER JOIN CLARITY_EMP ON X_CLARITY_SUBSCRIB.USER_NUMBER_ID=CLARITY_EMP.USER_ID " +
+                    "LEFT JOIN REPORT_INFO ON X_CLARITY_SUBSCRIB.SUBSCRIBED_REPT_ID = REPORT_INFO.REPORT_INFO_ID " +
+                    "LEFT JOIN TEMPLATE_INFO ON REPORT_INFO.REPORT_ID  = TEMPLATE_INFO.REPORT_ID " +
+                    "LEFT JOIN CLARITY_RPT ON REPORT_INFO.REPORT_ID = CLARITY_RPT.ASSOC_REPORT_ID " +
+                    "WHERE CLARITY_EMP.SYSTEM_LOGIN=? AND X_FILE_NAME=? AND CLARITY_RPT.REPORT_NAME IS NOT NULL AND TEMPLATE_INFO.STATUS_C = 0");
+                ocomm.Parameters.Add("login", OdbcType.VarChar).Value = username;
+                ocomm.Parameters.Add("filename", OdbcType.VarChar).Value = fname;
+                ocomm.Connection = oconn;
+
+                oconn.Open();
+                OdbcDataReader odr = ocomm.ExecuteReader();
+
+                reportCount = odr.HasRows;
+            }
+            return reportCount;
         }
     }
 }
