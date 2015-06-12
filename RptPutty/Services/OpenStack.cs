b8a5676c701d793e4206df912524c0f2d8f273f4
Adrian La Duca <aladuca@summatus.net>
2015-06-12 11:00:30 -0400

    Initial implementation of OpenStack Swift report links on status page

---
+++ RptPutty/Services/OpenStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+// openstack-sdk-dotnet @ https://github.com/stackforge/openstack-sdk-dotnet
+using OpenStack;
+using OpenStack.Identity;
+using OpenStack.Storage;
+
+namespace RptPutty.Services
+{
+    public class OpenStack
+    {
+        static public List<String> getExports()
+        {
+            // Login
+            var credential = new OpenStackCredential(new Uri("http://10.2.4.20:5000/v2.0"), "rptdynamo", "3IF0unuGJ2Qi4kOtYq1M", "itreporting", "RegionOne");
+            var client = OpenStackClientFactory.CreateClient(credential);
+            client.Connect().Wait();
+            // Get public URL
+            var swiftPublicEndpoint = credential.ServiceCatalog.GetPublicEndpoint("swift", "RegionOne");
+            // Get Object List
+            var swiftClient =  client.CreateServiceClient<IStorageServiceClient>();
+            var swiftListObjects = swiftClient.ListStorageObjects("exports");
+            swiftListObjects.Wait();
+            var swiftObjects = swiftListObjects.Result;
+
+            List<string> SwiftObjURL = new List<string>();
+            foreach (var sObject in swiftObjects)
+            {
+                SwiftObjURL.Add(swiftPublicEndpoint + "/" + "exports" + "/" + sObject.FullName);
+            }
+            return SwiftObjURL;
+        }
+    }
+}
